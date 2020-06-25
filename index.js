5143918286643dd7553252e296fc781e793b8a1e
Christopher Zell <christopher.zell@gmail.com>
2020-06-25 16:30:50 -0400

    changed permissions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'chris',
+  user: 'me',
   host: 'localhost',
   database: 'students',
   password: '',
